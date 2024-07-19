719f72a22bbfb476a72fa02dd1d17434babdc658
Thomás Gomes Almeida <tom.gomes@hotmail.com>
2024-07-19 09:03:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Meu primeiro Dashboard')
+st.title('Meu primeiro Luanzinho')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
